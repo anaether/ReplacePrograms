40b8c18b62af04b3bab4efe14abf2992d371e142
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-16 19:26:49 +0100

    Adding ScreenUtils for UI

---
+++ ReplacePrograms/ReplacePrograms/Utils/ScreenUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReplacePrograms.Utils
+{
+    public static class ScreenUtils
+    {
+        /// <summary>
+        /// Calculate Centerposition with current Window.
+        /// </summary>
+        /// <param name="screenWidth">Width of the monitor screen.</param>
+        /// <param name="windowWidth">Width of the program screen.</param>
+        /// <param name="screenHeight">Heigth of the monitor screen.</param>
+        /// <param name="windowHeight">Heigth of the program screen.</param>
+        /// <returns></returns>
+        public static (int x, int y) CenterPosition(int screenWidth, int windowWidth, int screenHeight, int windowHeight)
+        {
+            int x = ((int)((screenWidth - windowWidth) / 2));
+            int y = ((int)((screenHeight - windowHeight) / 2));
+
+            return (x, y);
+        }
+    }
+}

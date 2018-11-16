bd8d170fab9a5ec5534e02fa58b80120c74eb946
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-16 19:11:59 +0100

    Update Program.cs call for UI

--- ReplacePrograms/ReplacePrograms/Program.cs
+++ ReplacePrograms/ReplacePrograms/Program.cs
@@ -1,22 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ReplacePrograms.UI;
 
 namespace ReplacePrograms
 {
-    static class Program
+    public static class Program
     {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainUI());
         }
     }
 }

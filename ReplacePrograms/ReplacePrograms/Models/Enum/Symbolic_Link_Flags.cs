a361dee158f731be359c0eab6ba3c723693768ed
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-16 20:01:58 +0100

    Update general

---
+++ ReplacePrograms/ReplacePrograms/Models/Enum/Symbolic_Link_Flags.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReplacePrograms.Models.Enum
+{
+    [Flags]
+    public enum SYMBOLIC_LINK_FLAG
+    {
+        File = 0,
+        Directory = 1,
+        AllowUnprivilegedCreate = 2
+    }
+}

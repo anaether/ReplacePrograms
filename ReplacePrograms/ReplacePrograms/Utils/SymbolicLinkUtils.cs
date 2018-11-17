8ebc6e5e987b438d54337e574fbc8fb327edc6b5
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-17 13:48:32 +0100

    Added back SymbolicLinksUtils class

---
+++ ReplacePrograms/ReplacePrograms/Utils/SymbolicLinkUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using IWshRuntimeLibrary;
+
+namespace ReplacePrograms.Utils
+{
+    public static class SymbolicLinkUtils
+    {
+        public static void CreateSymbolicLinks(string source, string destination, string rootpath)
+        {
+            string symbol = "pushd " + rootpath + " & " + "/c mklink /D " + ((char)34) + source + ((char)34) + " " + ((char)34) + destination + ((char)34) + " & exit";
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.Verb = "runas";
+            info.Arguments = symbol;
+
+            Process process = new Process();
+            process.StartInfo = info;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        /// <summary>
+        /// Create Shortcut
+        /// </summary>
+        /// <param name="shortcutDir">Source directory where the shortcut will be saved.</param>
+        /// <param name="targetPath">Target directory where the source for the shortcut is located.</param>
+        public static void CreateShortcut(string shortcutDir, string targetPath)
+        {
+            string path = shortcutDir + ".lnk";
+            WshShellClass shell = new WshShellClass();
+            WshShortcut shortcut = (WshShortcut)shell.CreateShortcut(path);
+
+            shortcut.TargetPath = targetPath;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath);
+            shortcut.Arguments = "Argument";
+            shortcut.Description = "This is a Shortcut";
+
+            shortcut.Save();
+        }
+    }
+}

6789c97e1c7089919860d5be251cb3defc12082b
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-17 13:15:04 +0100

    Added ProgramUtils

---
+++ ReplacePrograms/ReplacePrograms/Utils/ProgramUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace ReplacePrograms.Utils
+{
+    public static class ProgramUtils
+    {
+        /// <summary>
+        /// Check if program is started with admin rights.
+        /// </summary>
+        /// <returns></returns>
+        public static bool HasAdministratorRights()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        /// <summary>
+        /// Restart Application when its not started with admin rights.
+        /// </summary>
+        public static void RestartApplication()
+        {
+
+
+            try
+            {
+                Environment.Exit(0);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Source);
+            }
+        }
+    }
+}

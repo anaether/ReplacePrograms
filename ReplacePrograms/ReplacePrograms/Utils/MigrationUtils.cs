d31f19760dadecdfb2ab2aa7a423f765c7c6dfcd
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-17 13:48:01 +0100

    Make MigrationTools working with Threading.Tasks

--- ReplacePrograms/ReplacePrograms/Utils/MigrationUtils.cs
+++ ReplacePrograms/ReplacePrograms/Utils/MigrationUtils.cs
@@ -1,69 +1,33 @@
 ﻿using System;
-using System.IO;
-using System.Diagnostics;
 using System.Threading.Tasks;
-using IWshRuntimeLibrary;
+using System.Collections.Generic;
 using ReplacePrograms.Models;
 
 namespace ReplacePrograms.Utils
 {
     class MigrationUtils
     {
-        public static void CreateSymbolicLinksAsync(string source, string destination, string rootpath)
+        public static async Task Proceed(List<Migration> data)
         {
-            string symbol = "pushd " + rootpath + " & " + "/c mklink /D " + ((char)34) + source + ((char)34) + " " + ((char)34) + destination + ((char)34) + " & exit";
+            for (int i = 0; i < data.Count; i++)
+            {
+                string SourceFolderName = FolderUtils.GetFolderNameOfPath(data[i].Source);
+                string RootPath = FolderUtils.GetFullPathName(data[i].Source);
+                string DestinationFolderName = data[i].Destination + "" + SourceFolderName;
 
-            ProcessStartInfo info = new ProcessStartInfo();
-            info.FileName = "cmd.exe";
-            info.WindowStyle = ProcessWindowStyle.Hidden;
-            info.Verb = "runas";
-            info.Arguments = symbol;
+                // Copy all stuff to new directory.
+                FolderUtils.CopyDirectory(data[i].Source, DestinationFolderName);
 
-            Process process = new Process();
-            process.StartInfo = info;
-            process.Start();
-            process.WaitForExit();
-            Console.WriteLine("Symbolic Link has been created.");
-        }
+                // Remove old folder to can create new Symbolic Links.
+                FolderUtils.DeleteDirectory(data[i].Source);
 
-        /// <summary>
-        /// Create Shortcut
-        /// </summary>
-        /// <param name="shortcutDir">Source directory where the shortcut will be saved.</param>
-        /// <param name="targetPath">Target directory where the source for the shortcut is located.</param>
-        public static void CreateShortcut(string shortcutDir, string targetPath)
-        {
-            string path = shortcutDir + ".lnk";
-            WshShellClass shell = new WshShellClass();
-            WshShortcut shortcut = (WshShortcut)shell.CreateShortcut(path);
-
-            shortcut.TargetPath = targetPath;
-            shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath);
-            shortcut.Arguments = "Argument";
-            shortcut.Description = "This is a Shortcut";
-
-            shortcut.Save();
-        }
-
-        public static async Task ProceedMigration(string source, string destination)
-        {
-            string SourceFolderName = FolderUtils.GetFolderNameOfPath(source);
-            string RootPath = FolderUtils.GetFullPathName(source);
-            string DestinationFolderName = destination + "" + SourceFolderName;
-
-            // Copy all stuff to new directory.
-            FolderUtils.CopyDirectory(source, DestinationFolderName);
-
-            // Remove old folder to can create new Symbolic Links.
-            FolderUtils.DeleteDirectory(source);
-
-            // Create new symbolic links from (source folder) to the new folder.
-            // CreateSymbolicLinksAsync(source, DestinationFolderName);
-            //CreateSymbolicLinksAsync(source, DestinationFolderName, RootPath);
-
-            //CreateShortcut(source, DestinationFolderName);
-            CreateSymbolicLinksAsync(source, DestinationFolderName, RootPath);
-            Console.WriteLine("symolic link created for {0} <<===>> {1}", source, DestinationFolderName);
+                if (data[i].CreateSymbolicLink)
+                {
+                    // Create new symboliclink.
+                    SymbolicLinkUtils.CreateSymbolicLinks(data[i].Source, DestinationFolderName, RootPath);
+                    Console.WriteLine("symolic link created for {0} <<===>> {1}", data[i].Source, DestinationFolderName);
+                }
+            }
 
             await Task.Delay(1);
         }

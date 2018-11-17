b49350e96e0ae94d534dd49afabd56327ac8be68
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-17 13:47:38 +0100

    Update MainUI

--- ReplacePrograms/ReplacePrograms/UI/MainUI.cs
+++ ReplacePrograms/ReplacePrograms/UI/MainUI.cs
@@ -3,13 +3,14 @@ using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using ReplacePrograms.Utils;
+using ReplacePrograms.Models;
 
 namespace ReplacePrograms.UI
 {
     public partial class MainUI : Form
     {
-        private Size ProgramSize = new Size(381, 151);
         private string SourcePath = "";
         private string DestinationPath = "";
 
@@ -17,13 +18,14 @@ namespace ReplacePrograms.UI
         {
             InitializeComponent();
 
+            // For testing:
+            this.button2.Visible = this.button2.Enabled = false;
+            this.button3.Visible = this.button3.Enabled = false;
+
             // UI Settings
-            this.Text = "Replace Programs";
             this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
-            this.MinimumSize = ProgramSize;
-            this.MaximumSize = ProgramSize;
-            this.Size = ProgramSize;
             this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.CenterToScreen();
         }
 
@@ -57,17 +59,65 @@ namespace ReplacePrograms.UI
 
         private void button1_Click(object sender, System.EventArgs e)
         {
-            Console.WriteLine((ProgramUtils.HasAdministratorRights()) ? "True" : "False");
+            //Console.WriteLine((ProgramUtils.HasAdministratorRights()) ? "True" : "False");
 
-            if (this.SourcePath.Length > 0 && this.DestinationPath.Length > 0)
+            if (this.listboxSource.Items.Count == this.listBoxDestination.Items.Count && (this.listboxSource.Items.Count > 0 && this.listBoxDestination.Items.Count > 0))
             {
                 // Create a new Task to run the complete migration behind and to dont freeze the UI.
-                Task.Run(() => MigrationUtils.ProceedMigration(this.SourcePath, this.DestinationPath));
+                Task.Run(() => MigrationUtils.Proceed(ConvertData()));
+
+                // When we are done we can empty the listboxes
+                this.listboxSource.Items.Clear();
+                this.listBoxDestination.Items.Clear();
             }
             else
             {
                 // Display Error Message to user.
                 MessageBox.Show("Sie haben entweder Source/Destination Ordner nicht ausgewählt.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private List<Migration> ConvertData()
+        {
+            List<Migration> data = new List<Migration>();
+
+            if(this.listboxSource.Items.Count == this.listBoxDestination.Items.Count)
+            {
+                for(int i = 0; i < this.listboxSource.Items.Count; i++)
+                {
+                    Migration current = new Migration(this.listboxSource.Items[i].ToString(), this.listBoxDestination.Items[i].ToString(), true);
+                    data.Add(current);
+                }
+            }
+
+            return data;
+        }
+
+        private void AddSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            folderBrowser.FileName = "Folder Selection.";
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                this.listboxSource.Items.Add(Path.GetDirectoryName(folderBrowser.FileName));
+            }
+        }
+
+        private void AddDestination_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            folderBrowser.FileName = "Folder Selection.";
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                this.listBoxDestination.Items.Add(Path.GetDirectoryName(folderBrowser.FileName));
             }
         }
     }

f2e4d09daab223186ed111894472b8b275383489
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-17 13:15:12 +0100

    Added ProcessUtils

---
+++ ReplacePrograms/ReplacePrograms/Utils/ProcessUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplacePrograms.Utils
+{
+    public static class ProcessUtils
+    {
+        public static async Task EliminateProcesses(List<string> applications)
+        {
+            if (!ProgramUtils.HasAdministratorRights())
+            {
+                ProgramUtils.RestartApplication();
+            }
+
+            for(int i = 0; i < applications.Count; i++)
+            {   
+
+            }
+        }
+    }
+}

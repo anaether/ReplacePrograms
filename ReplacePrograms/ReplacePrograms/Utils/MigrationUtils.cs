0c7ad7ea8b558c050964090a46409a884272dbf9
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-17 10:02:55 +0100

    Update MigrationUtils
    Fix bug, that console needs to close after excecution.

--- ReplacePrograms/ReplacePrograms/Utils/MigrationUtils.cs
+++ ReplacePrograms/ReplacePrograms/Utils/MigrationUtils.cs
@@ -9,7 +9,7 @@ namespace ReplacePrograms.Utils
     {
         public static void CreateSymbolicLinksAsync(string source, string destination, string rootpath)
         {
-            string symbol = "pushd " + rootpath + " & " + "/c mklink /D " + ((char)34) + source + ((char)34) + " " + ((char)34) + destination + ((char)34); // + " & exit";
+            string symbol = "pushd " + rootpath + " & " + "/c mklink /D " + ((char)34) + source + ((char)34) + " " + ((char)34) + destination + ((char)34) + " & exit";
 
             ProcessStartInfo info = new ProcessStartInfo();
             info.FileName = "cmd.exe";
@@ -21,7 +21,7 @@ namespace ReplacePrograms.Utils
             process.StartInfo = info;
             process.Start();
             process.WaitForExit();
-            Console.WriteLine("Process finished");
+            Console.WriteLine("Symbolic Link has been created.");
         }
 
         /// <summary>

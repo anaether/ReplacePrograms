b8ba8960961b2e96507c1c78ae588bb910a0ae44
cRUSHER-HLG <35463368+cRUSHER-HLG@users.noreply.github.com>
2018-11-17 13:47:31 +0100

    Update

--- ReplacePrograms/ReplacePrograms/UI/MainUI.Designer.cs
+++ ReplacePrograms/ReplacePrograms/UI/MainUI.Designer.cs
@@ -33,11 +33,15 @@
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.listboxSource = new System.Windows.Forms.ListBox();
+            this.listBoxDestination = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(493, 273);
+            this.button1.Location = new System.Drawing.Point(468, 286);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(60, 23);
             this.button1.TabIndex = 0;
@@ -47,7 +51,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(482, 215);
+            this.button2.Location = new System.Drawing.Point(457, 228);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(71, 23);
             this.button2.TabIndex = 1;
@@ -57,7 +61,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(482, 244);
+            this.button3.Location = new System.Drawing.Point(457, 257);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(71, 23);
             this.button3.TabIndex = 2;
@@ -68,7 +72,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(98, 12);
+            this.label1.Location = new System.Drawing.Point(188, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 3;
@@ -77,24 +81,64 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(427, 12);
+            this.label2.Location = new System.Drawing.Point(391, 29);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Destination";
             // 
+            // listboxSource
+            // 
+            this.listboxSource.FormattingEnabled = true;
+            this.listboxSource.Location = new System.Drawing.Point(13, 45);
+            this.listboxSource.Name = "listboxSource";
+            this.listboxSource.Size = new System.Drawing.Size(216, 264);
+            this.listboxSource.TabIndex = 5;
+            // 
+            // listBoxDestination
+            // 
+            this.listBoxDestination.FormattingEnabled = true;
+            this.listBoxDestination.Location = new System.Drawing.Point(235, 45);
+            this.listBoxDestination.Name = "listBoxDestination";
+            this.listBoxDestination.Size = new System.Drawing.Size(216, 264);
+            this.listBoxDestination.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Add Source";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.AddSource_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(235, 16);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Add Destination";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.AddDestination_Click);
+            // 
             // MainUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 308);
+            this.ClientSize = new System.Drawing.Size(537, 321);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBoxDestination);
+            this.Controls.Add(this.listboxSource);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "MainUI";
-            this.Text = "Form1";
+            this.Text = "Replace Programs";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +151,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listboxSource;
+        private System.Windows.Forms.ListBox listBoxDestination;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
